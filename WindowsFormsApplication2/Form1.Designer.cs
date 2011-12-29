88807d7f746afabe56c3a312a709a0970b844ede
Ajith Antony <ajith.antony@gmail.com>
2011-12-29 05:09:59 -0600

    Intial work on the keyboard timings tool

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textEntry = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textEntry
+            // 
+            this.textEntry.Location = new System.Drawing.Point(24, 13);
+            this.textEntry.Multiline = true;
+            this.textEntry.Name = "textEntry";
+            this.textEntry.Size = new System.Drawing.Size(398, 466);
+            this.textEntry.TabIndex = 0;
+            this.textEntry.KeyDown += new KeyEventHandler(this.textEntry_KeyDown);
+            this.textEntry.KeyUp += new KeyEventHandler(this.textEntry_KeyUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 514);
+            this.Controls.Add(this.textEntry);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textEntry;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2 {
+    public class KeyRecord {
+        public DateTime down;
+        public DateTime up;
+        private int duration;
+        int delay;
+        public Keys key;
+
+        public KeyRecord(Keys key) {
+            this.key = key;
+ 
+        }
+        public String toAutoIT() {
+
+            StringBuilder r = new StringBuilder();
+            r.AppendFormat("Opt(\"SendKeyDelay\", {0})\n", this.Duration);
+            r.AppendFormat("Send(\"{{ASC {0}}}\")\n", this.key);
+
+            return r.ToString();
+
+        }
+
+        public int Duration {
+            get{
+                TimeSpan ts = up - down;
+                duration =  Convert.ToInt32(ts.TotalMilliseconds);
+                return duration;
+            }
+            set {
+                duration = value;
+            }
+        }
+    }
+
+    public partial class Form1 : Form {
+        public List<KeyRecord> allrecords  = new List<KeyRecord>();
+        Dictionary<Keys, KeyRecord> lastDowns = new Dictionary<Keys, KeyRecord>();
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void textEntry_KeyDown(object sender, System.Windows.Forms.KeyEventArgs ke) {
+
+            DateTime downTime = DateTime.Now;
+            System.Console.WriteLine("{1} Down at {0}", downTime.Millisecond, ke.KeyCode);
+            if (!lastDowns.ContainsKey(ke.KeyCode)) {
+                KeyRecord k = new KeyRecord(ke.KeyCode);
+                k.down = downTime;
+                lastDowns.Add(k.key, k);
+            } else {
+                System.Console.WriteLine("Duplicate Down");
+            }
+        }
+
+        private void textEntry_KeyUp(object sender, System.Windows.Forms.KeyEventArgs ke) {
+            DateTime upTime = DateTime.Now;
+            System.Console.WriteLine("{1} Up   at {0}", upTime.Millisecond, ke.KeyCode);
+            if (lastDowns.ContainsKey(ke.KeyCode)) {
+                KeyRecord k = lastDowns[ke.KeyCode];
+                k.up = upTime;
+                lastDowns.Remove(k.key);
+                System.Console.WriteLine(k.toAutoIT());
+            }
+
+        }
+
+
+    }
+}
